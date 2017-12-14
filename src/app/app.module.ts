834924512cb6e273e1815e281b9a4ac462ec367f
tamonwan <tamonwan.ning@g.swu.ac.th>
2017-12-14 17:30:34 +0700

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './components/home/home.component';
+import {RouterModule} from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
